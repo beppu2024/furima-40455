037a84d340b404645b6e3b9331abc05de0904f15
beppu2024 <162876356+beppu2024@users.noreply.github.com>
2024-04-10 11:31:01 +0900

    Rubocopを実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
   end
 
@@ -20,5 +19,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
