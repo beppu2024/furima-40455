78edea579c21fda3b0838e12979d3a3b2aa7ad5c
beppu2024 <162876356+beppu2024@users.noreply.github.com>
2024-04-15 18:04:44 +0900

    コメントアウトの修正、コメントの削除、orderメソッドの追加、pathの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,17 +65,17 @@ RSpec.describe Item, type: :model do
       it '価格が¥10,000,000以上の場合は出品できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
       it '価格は半角数値以外では出品できない' do
         @item.price = '１２３４５'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is invalid')
       end
-      it 'userが紐付いていない場合は登録できない'do
-      @item.user = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("User must exist")
+      it 'userが紐付いていない場合は登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
