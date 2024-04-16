e71d21efeb7305fc447d3d3af10d2ca95f13ad31
beppu2024 <162876356+beppu2024@users.noreply.github.com>
2024-04-16 17:45:37 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
+    return if current_user == @item.user
+
+    redirect_to root_path
   end
 
   def update
