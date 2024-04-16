30661ec83565ae2d641f3b9387a8ea087efd7c55
beppu2024 <162876356+beppu2024@users.noreply.github.com>
2024-04-16 14:51:40 +0900

    コードの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.includes(:user).find(params[:id])
+    @item = Item. @item = Item.find(params[:id])
   end
 
   private
